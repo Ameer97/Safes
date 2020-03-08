2858b711b906abc5ee9b65c0e13e6aac020d0591
Ameer Al Issa <ameerwisam97@gmail.com>
2020-03-08 07:33:32 +0300

    add migration, some fixes (make nullablew of dateto dateform), delete status from staticbox, staticboxreuse

---
+++ Safes.DAL/Migrations/20200308034245_test.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Safes.DAL.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "StaticBoxes");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateTo",
+                table: "StaticBoxReuses",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateFrom",
+                table: "StaticBoxReuses",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "StaticBoxReuses",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MeditorId",
+                table: "StaticBoxReuses",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "StaticBoxes",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StaticBoxReuses_MeditorId",
+                table: "StaticBoxReuses",
+                column: "MeditorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StaticBoxReuses_Meditors_MeditorId",
+                table: "StaticBoxReuses",
+                column: "MeditorId",
+                principalTable: "Meditors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StaticBoxReuses_Meditors_MeditorId",
+                table: "StaticBoxReuses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_StaticBoxReuses_MeditorId",
+                table: "StaticBoxReuses");
+
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "StaticBoxReuses");
+
+            migrationBuilder.DropColumn(
+                name: "MeditorId",
+                table: "StaticBoxReuses");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "StaticBoxes");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateTo",
+                table: "StaticBoxReuses",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateFrom",
+                table: "StaticBoxReuses",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Status",
+                table: "StaticBoxes",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
